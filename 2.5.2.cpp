03b67f2cda67a9adb3372beb046749243e1e6243
Nikita <torsjan1@rambler.ru>
2015-11-13 13:00:39 +0400

    2.5.2(mess)

---
+++ 2.5.2.cpp
@@ -0,0 +1,11 @@
+#include "sdt.h"
+//Данная программа является учебной.
+int main()
+{
+
+int n,m,sum;
+cout << "Vvedite 2 chisla please";
+cin >> n >>m  ;
+sum=n+m;
+cout<<"summa"<<" "<<sum<<"\n ";
+}
