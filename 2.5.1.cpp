517d1ae31b3a1e5fd69c2422d813291fde7029c0
Nikita <torsjan1@rambler.ru>
2015-11-13 12:51:52 +0400

    2.5.1(razn)

---
+++ 2.5.1.cpp
@@ -0,0 +1,13 @@
+#include "sdt.h"
+
+int main()
+{
+
+int n,m,sum,razn;
+cout << "Vvedite 2 chisla please";
+cin >> n >>m  ;
+sum=n+m;
+razn=n-m;
+cout<<"summa"<<" "<<sum<<"\n ";
+cout<<"raznost"<<" "<<razn<<"\n ";
+}
