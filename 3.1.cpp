4136432fa64d9317d82f2f4af717d2e757de4d0a
Nikita <torsjan1@rambler.ru>
2015-11-27 12:04:23 +0400

    3.1

---
+++ 3.1.cpp
@@ -0,0 +1,14 @@
+#include "sdt.h"
+//Данная программа является учебной.
+int main()
+{
+
+int n,m,sum,razn,proizv;
+cout << "Vvedite 2 chisla please";
+cin >> n >>m  ;
+sum=n+m;
+razn=n-m;
+proizv=n*m;
+cout<<"summa"<<" "<<sum<<"\n ";
+cout<<"raznost"<<" "<<razn<<"\n ";
+cout<<"proizv"<<" "<<proizv;
